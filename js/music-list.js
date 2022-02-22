030c0c7cd8244c0d7b79f54c9a90b52262b75df9
dawenxt <gacerdarwin265@gmail.com>
2022-02-22 20:55:53 +0800

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+  {
+    name: "For you",
+    artist: "Trevor Daniel",
+    img: "music1.jpg",
+    src: "music1"
+  },
+  {
+    name: "Enchanted",
+    artist: "Taylor Swift",
+    img: "music2",
+    src: "music2"
+  },
+  {
+    name: "Love Don't Change",
+    artist: "Jeremih",
+    img: "",
+    src: "music3"
+  },
+  {
+    name: "Dito kana muna",
+    artist: "Jroa",
+    img: "music4",
+    src: "music4"
+  },
+  {
+    name: "Ikaw lang",
+    artist: "Nobita",
+    img: "music5",
+    src: "music5"
+  },
+  {
+    name: "Nangangamba",
+    artist: "Zack tabudlo",
+    img: "music6",
+    src: "music6"
+  },
+  {
+    name: "10,000 hours",
+    artist: "Justin Bieber",
+    img: "music7",
+    src: "music7"
+  },
+  {
+    name: "Need you most",
+    artist: "Kid Laroi",
+    img: "music8",
+    src: "music8"
+  },
+  {
+    name: "Jackie Chan",
+    artist: "Tiësto & Dzeko ft. Preme & Post Malone",
+    img: "music9",
+    src: "music9"
+  },
+   {
+    name: "Selfish",
+    artist: "Kid Laroi",
+    img: "music10",
+    src: "music10"
+  },
+
+];
